2133f22bb2b80cab3b1db9dbf501d98fec4bdc05
posixpositive <37414199+bensonjohnson@users.noreply.github.com>
2022-09-19 18:07:32 -0600

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 chmod -R 777 /home/steam/
 /home/steam/steamcmd.sh +force_install_dir /home/steam/steamworks_sdk +login anonymous +@sSteamCmdForcePlatformType windows +app_update 1007 +quit
 /home/steam/steamcmd.sh @sSteamCmdForcePlatformType windows +force_install_dir /home/steam/sw +login anonymous +app_update 1247090  +quit
