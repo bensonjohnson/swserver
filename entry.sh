93f078e14a462f771d00a068d65670545d6f8f97
posixpositive <37414199+bensonjohnson@users.noreply.github.com>
2022-11-14 05:03:54 -0700

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -3,5 +3,6 @@ chmod -R 777 /home/steam/
 /home/steam/steamcmd.sh +force_install_dir /home/steam/steamworks_sdk +login anonymous +@sSteamCmdForcePlatformType windows +app_update 1007 +quit
 /home/steam/steamcmd.sh @sSteamCmdForcePlatformType windows +force_install_dir /home/steam/sw +login anonymous +app_update 1247090  +quit
 cp /home/steam/steamworks_sdk/*64.dll /home/steam/sw
-winecfg
-xvfb-run wine /home/steam/sw/server64.exe
+/opt/wine-stable/bin/winecfg
+xvfb-run /opt/wine-stable/bin/wine /home/steam/sw/server64.exe
+
