9915283b56d395594a32ae324246aa244f67a7f8
posixpositive <bensonjohnson@me.com>
2023-03-31 12:27:34 -0600

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod -R 755 /home/steam/
 /home/steam/steamcmd.sh +force_install_dir /home/steam/steamworks_sdk +login anonymous +@sSteamCmdForcePlatformType windows +app_update 1007 +quit
-/home/steam/steamcmd.sh @sSteamCmdForcePlatformType windows +force_install_dir /home/steam/sw +login anonymous +app_update 1247090  +quit
+/home/steam/steamcmd.sh @sSteamCmdForcePlatformType windows +force_install_dir /home/steam/sw +login anonymous +app_update 1247090 +validate +quit
 cp /home/steam/steamworks_sdk/*64.dll /home/steam/sw
 winecfg
 xvfb-run wine /home/steam/sw/server64.exe
