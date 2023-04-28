e8b4d80a62387a8052da0f6c629b8ac27c60a4d6
posixpositive <bensonjohnson@me.com>
2023-04-28 13:02:13 -0600

    Update base to ubuntu 23.04

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod -R 755 /home/steam/
+chmod -R 775 /home/steam/
 /home/steam/steamcmd.sh +force_install_dir /home/steam/steamworks_sdk +login anonymous +@sSteamCmdForcePlatformType windows +app_update 1007 +quit
 /home/steam/steamcmd.sh @sSteamCmdForcePlatformType windows +force_install_dir /home/steam/sw +login anonymous +app_update 1247090 validate +quit
 cp /home/steam/steamworks_sdk/*64.dll /home/steam/sw
